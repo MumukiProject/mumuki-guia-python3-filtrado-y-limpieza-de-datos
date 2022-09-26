87e9f5066fdbd6d7f99fef1b3a09a9e45ee71adc
franco@mumuki.org <franco@mumuki.org>
2022-09-26 16:23:42 +0000

    Mumuki Export on 2022-09-26 16:23:42 +0000

---
+++ 00003_A vos te conozco/test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+class Test(unittest.TestCase):
+  def test_genera_un_dataframe(self):
+    self.assertEquals(type(sauces), pd.DataFrame)
+    
+  def test_sauces_solo_contiene_sauces(self):
+    self.assertEquals(len(sauces), 10)
