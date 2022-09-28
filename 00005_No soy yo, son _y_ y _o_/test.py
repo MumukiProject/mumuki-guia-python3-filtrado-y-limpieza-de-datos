c0007bb9056ff2bdbc3c6392f74aa0c0b5c5a240
franco@mumuki.org <franco@mumuki.org>
2022-09-28 13:54:08 +0000

    Mumuki Export on 2022-09-28 13:54:08 +0000

--- 00005_No soy yo, son _y_ y _o_/test.py
+++ 00005_No soy yo, son _y_ y _o_/test.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
 class Test(unittest.TestCase):
+ def setUp(self):
+    self.resultado = (
+      #...content...#
+    )
+  
   def test_genera_un_dataframe(self):
-    self.assertEquals(type(sauces), pd.DataFrame)
+    self.assertEquals(type(self.resultado), pd.DataFrame)
     
   def test_sauces_solo_contiene_sauces(self):
-    self.assertEquals(len(sauces), 1)
-    self.assertEquals(sauces.iloc[0]["tree_id"], 13003665, "Debería haber encontrado al árbol 13003665")
+    self.assertEquals(len(self.resultado), 1)
+    self.assertEquals(self.resultado.iloc[0]["tree_id"], 13003665, "Debería haber encontrado al árbol 13003665")
     
