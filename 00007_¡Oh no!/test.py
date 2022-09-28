03c40fa92cfb0749ead3a594013dd15f30f11d60
franco@mumuki.org <franco@mumuki.org>
2022-09-28 16:27:53 +0000

    Mumuki Export on 2022-09-28 16:27:53 +0000

--- 00007_¡Oh no!/test.py
+++ 00007_¡Oh no!/test.py
@@ -10,10 +10,11 @@ class Test(unittest.TestCase):
     self.assertEquals(type(self.resultado), pd.DataFrame)
     
   def test_no_contiene_fresnos(self):
+    tree_ids = list(self.resultado.iloc["tree_id"])
+ 
     self.assertEquals(len(self.resultado), 5)
-    
-    self.assertEquals(4101876 in list(self.resultado.iloc["tree_id"]), "Debería haber encontrado al árbol 4101876")
-    self.assertEquals(142527 in list(self.resultado.iloc["tree_id"]), "Debería haber encontrado al árbol 142527")
-    self.assertEquals(24002305 in list(self.resultado.iloc["tree_id"]), "Debería haber encontrado al árbol 24002305")
-    self.assertEquals(2003708 in list(self.resultado.iloc["tree_id"]), "Debería haber encontrado al árbol 2003708")
-    self.assertEquals(13003665 in list(self.resultado.iloc["tree_id"]), "Debería haber encontrado al árbol 13003665")    
+    self.assertEquals(142527 in tree_ids, "Debería haber encontrado al árbol 142527")
+    self.assertEquals(24002305 in tree_ids, "Debería haber encontrado al árbol 24002305")
+    self.assertEquals(2003708 in tree_ids, "Debería haber encontrado al árbol 2003708")
+    self.assertEquals(13003665 in tree_ids, "Debería haber encontrado al árbol 13003665")    
+ 
