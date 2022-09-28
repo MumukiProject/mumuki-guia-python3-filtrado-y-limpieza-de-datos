9a2250d8c42a5241b68481a952363f54a703e8a4
franco@mumuki.org <franco@mumuki.org>
2022-09-28 16:27:11 +0000

    Mumuki Export on 2022-09-28 16:27:11 +0000

--- 00007_¡Oh no!/test.py
+++ 00007_¡Oh no!/test.py
@@ -11,5 +11,9 @@ class Test(unittest.TestCase):
     
   def test_no_contiene_fresnos(self):
     self.assertEquals(len(self.resultado), 5)
-    self.assertEquals(self.resultado.iloc[0]["tree_id"], 13003665, "Debería haber encontrado al árbol 13003665")
-   
+    
+    self.assertEquals(4101876 in list(self.resultado.iloc["tree_id"]), "Debería haber encontrado al árbol 4101876")
+    self.assertEquals(142527 in list(self.resultado.iloc["tree_id"]), "Debería haber encontrado al árbol 142527")
+    self.assertEquals(24002305 in list(self.resultado.iloc["tree_id"]), "Debería haber encontrado al árbol 24002305")
+    self.assertEquals(2003708 in list(self.resultado.iloc["tree_id"]), "Debería haber encontrado al árbol 2003708")
+    self.assertEquals(13003665 in list(self.resultado.iloc["tree_id"]), "Debería haber encontrado al árbol 13003665")    
