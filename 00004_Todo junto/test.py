7f09828635063f981bba71674079b959ad109a61
franco@mumuki.org <franco@mumuki.org>
2022-09-28 13:55:12 +0000

    Mumuki Export on 2022-09-28 13:55:12 +0000

--- 00004_Todo junto/test.py
+++ 00004_Todo junto/test.py
@@ -7,10 +7,10 @@ class Test(unittest.TestCase):
     )
   
   def test_genera_un_dataframe(self):
-    self.assertEquals(type(resultado), pd.DataFrame)
+    self.assertEquals(type(self.resultado), pd.DataFrame)
     
   def test_solo_contiene_oleaceas_de_la_comuna_10(self):
-    self.assertEquals(len(resultado), 1)
-    self.assertEquals(resultado.iloc[0]["tree_id"], 182465, "Debería haber encontrado al árbol 182465")
+    self.assertEquals(len(self.resultado), 1)
+    self.assertEquals(self.resultado.iloc[0]["tree_id"], 182465, "Debería haber encontrado al árbol 182465")
     
     
