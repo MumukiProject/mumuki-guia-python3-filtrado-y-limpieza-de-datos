c0464363d57a56bd769ed3e71f732164aef8a453
franco@mumuki.org <franco@mumuki.org>
2022-09-28 13:21:14 +0000

    Mumuki Export on 2022-09-28 13:21:14 +0000

--- 00002_Un poco de filtro/test.py
+++ 00002_Un poco de filtro/test.py
@@ -7,7 +7,7 @@ class Test(unittest.TestCase):
     )
 
   def test_genera_un_dataframe(self):
-    self.assertEquals(type(resultado), pd.DataFrame)
+    self.assertEquals(type(self.resultado), pd.DataFrame)
     
   def test_devuelve_solo_los_arboles_de_palermo(self):
-    self.assertEquals(len(resultado), 1)
+    self.assertEquals(len(self.resultado), 1)
