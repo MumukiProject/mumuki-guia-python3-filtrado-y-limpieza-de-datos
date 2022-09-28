53619af52ae6ad17376580ba32cee0de09b2d253
franco@mumuki.org <franco@mumuki.org>
2022-09-28 13:37:14 +0000

    Mumuki Export on 2022-09-28 13:37:14 +0000

--- 00004_Todo junto/test.py
+++ 00004_Todo junto/test.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 class Test(unittest.TestCase):
   def test_genera_un_dataframe(self):
-    self.assertEquals(type(sauces), pd.DataFrame)
+    self.assertEquals(type(meliaceas_comuna_10), pd.DataFrame)
     
-  def test_sauces_solo_contiene_sauces(self):
-    self.assertEquals(len(sauces), 1)
-    self.assertEquals(sauces.iloc[0]["tree_id"], 13003665, "Debería haber encontrado al árbol 13003665")
+  def test_sauces_solo_contiene_meliaceas_de_la_comuna_10(self):
+    self.assertEquals(len(meliaceas_comuna_10), 1)
+    self.assertEquals(meliaceas_comuna_10.iloc[0]["tree_id"], 13003665, "Debería haber encontrado al árbol 13003665")
     
     
