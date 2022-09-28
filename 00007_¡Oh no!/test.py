2367cf85201e200e1a075728c2436be17afa715a
franco@mumuki.org <franco@mumuki.org>
2022-09-28 16:29:33 +0000

    Mumuki Export on 2022-09-28 16:29:33 +0000

--- 00007_¡Oh no!/test.py
+++ 00007_¡Oh no!/test.py
@@ -13,8 +13,8 @@ class Test(unittest.TestCase):
     tree_ids = list(self.resultado["tree_id"])
  
     self.assertEquals(len(self.resultado), 5)
-    self.assertEquals(142527 in tree_ids, "Debería haber encontrado al árbol 142527")
-    self.assertEquals(24002305 in tree_ids, "Debería haber encontrado al árbol 24002305")
-    self.assertEquals(2003708 in tree_ids, "Debería haber encontrado al árbol 2003708")
-    self.assertEquals(13003665 in tree_ids, "Debería haber encontrado al árbol 13003665")    
+    self.assertTrue(142527 in tree_ids, "Debería haber encontrado al árbol 142527")
+    self.assertTrue(24002305 in tree_ids, "Debería haber encontrado al árbol 24002305")
+    self.assertTrue(2003708 in tree_ids, "Debería haber encontrado al árbol 2003708")
+    self.assertTrue(13003665 in tree_ids, "Debería haber encontrado al árbol 13003665")    
  
