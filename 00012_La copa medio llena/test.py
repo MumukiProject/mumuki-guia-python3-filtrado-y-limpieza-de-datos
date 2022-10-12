2b76280f6f983b4254eff3e40475b89ba8933444
franco@mumuki.org <franco@mumuki.org>
2022-10-12 03:24:03 +0000

    Mumuki Export on 2022-10-12 03:24:03 +0000

--- 00012_La copa medio llena/test.py
+++ 00012_La copa medio llena/test.py
@@ -1,4 +1,7 @@
 class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_no_se_elimina_ningun_arbol(self):
+    self.assertTrue(len(arboles) == 10)
+    
+  def test_no_quedan_inclinaciones_en_nan(self):
+    self.assertTrue(arboles["inclination"].count() == 10)  
