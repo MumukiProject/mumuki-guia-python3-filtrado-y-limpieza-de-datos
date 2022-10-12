62bac2d55a465466b468a5f6b9f83a526b190d19
franco@mumuki.org <franco@mumuki.org>
2022-10-12 03:12:24 +0000

    Mumuki Export on 2022-10-12 03:12:24 +0000

--- 00011_Afinando la puntería/test.py
+++ 00011_Afinando la puntería/test.py
@@ -1,4 +1,26 @@
-class Test(unittest.TestCase):
+import pandas as pd
 
-  def test_description_example(self):
-    self.assertTrue(True)
+class Test(unittest.TestCase):
+  def setUp(self):
+    self.resultado = (
+      #...content...#
+    )
+  
+  def test_genera_un_dataframe(self):
+    self.assertEquals(type(self.resultado), pd.DataFrame)
+    
+  def test_contiene_menos_arboles(self):
+    self.assertTrue(len(self.resultado) == 5)
+    
+  def test_no_contiene_arboles_sin_latitud(self):
+    indexado = self.resultado.set_index("tree_id")
+    self.assertTrue(142527 not in indexado)
+    self.assertTrue(8087 not in indexado)
+    self.assertTrue(39000768 not in indexado)
+
+  def test_no_contiene_arboles_sin_longitud(self):
+    indexado = self.resultado.set_index("tree_id")
+    self.assertTrue(4101876 not in indexado)
+    self.assertTrue(8087 not in indexado)
+
+    
