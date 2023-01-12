3b90056df30f3b4f2bbded2158b5a447adca013f
franco@mumuki.org <franco@mumuki.org>
2023-01-12 16:03:57 +0000

    Mumuki Export on 2023-01-12 16:03:57 +0000

--- 00005_No soy yo, son _y_ y _o_/test.py
+++ 00005_No soy yo, son _y_ y _o_/test.py
@@ -12,7 +12,10 @@ class Test(unittest.TestCase):
   def test_solo_contiene_los_arboles_solicitados(self):
     self.assertEquals(len(self.resultado), 3)
     
-    self.assertTrue(8087 in list(self.resultado["tree_id"]), "Debería haber encontrado al árbol 8087")
+  def test_solo_contiene_los_arboles_de_la_comuna_10(self):
     self.assertTrue(142527 in list(self.resultado["tree_id"]), "Debería haber encontrado al árbol 142527")
     self.assertTrue(182465 in list(self.resultado["tree_id"]), "Debería haber encontrado al árbol 182465")
+    
+  def test_solo_contiene_los_arboles_de_balvanera(self):
+    self.assertTrue(8087 in list(self.resultado["tree_id"]), "Debería haber encontrado al árbol 8087")  
     
